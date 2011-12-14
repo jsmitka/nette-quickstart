7b491004d72be0cb3bf24520527ca4f87f43ea88
Jan Smitka <jan@smitka.org>
2011-12-14 13:57:29 +0100

    Začátek práce na HomepagePresenteru.

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -1,17 +1,23 @@
 <?php
 
 /**
- * Homepage presenter.
- *
- * @author     John Doe
- * @package    MyApplication
+ * Presenter pro úvodní stránku. Obsahuje seznam úkolů, které jsou uživateli přiřazeny.
  */
 class HomepagePresenter extends BasePresenter
 {
+	/** @var Nette\Database\Table\Selection */
+	private $tasks;
+
+	public function startup()
+	{
+		parent::startup();
+		$this->tasks = $this->getService('model')->getTasks();
+	}
+
 
 	public function renderDefault()
 	{
-		$this->template->anyVariable = 'any value';
+		$this->template->list = $this->tasks->order('created ASC');
 	}
 
 }
