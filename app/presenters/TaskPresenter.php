a667b1fd49672a18e1d51fa46b2880a67b96baa7
Jan Smitka <jan@smitka.org>
2011-12-17 10:33:40 +0100

    Přidán základ task formu.

--- app/presenters/TaskPresenter.php
+++ app/presenters/TaskPresenter.php
@@ -1,5 +1,7 @@
 <?php
 
+use Nette\Application\UI\Form;
+
 /**
  * Presenter, který zajišťuje výpis seznamů úkolů.
  */
@@ -28,5 +30,19 @@ class TaskPresenter extends BasePresenter
 	{
 		$this->template->taskList = $this->taskList;
 		$this->template->tasks = $this->taskList->related('task')->order('id');
+	}
+
+
+
+	protected function createComponentTaskForm()
+	{
+		$form = new Form();
+		$form->addText('text', 'Úkol:', 40, 100)
+			->addRule(Form::FILLED, 'Je nutné zadat text úkolu.');
+		$form->addSelect('user_id', 'Pro:', $this->model->getUsers()->fetchPairs('id', 'name'))
+			->setPrompt('- Vyberte -')
+			->addRule(Form::FILLED, 'Je nutné vybrat, komu je úkol přiřazen.');
+		$form->addSubmit('create', 'Vytvořit');
+		return $form;
 	}
 }
